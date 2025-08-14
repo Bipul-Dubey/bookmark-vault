b1d3a54a6377ce71393f18e615fcd9c44dd64ca4
Bipul Dubey <bipul0873@gmail.com>
2025-08-14 15:08:26 +0530

    fix: default theme dark

--- src/components/common/theme-provider.tsx
+++ src/components/common/theme-provider.tsx
@@ -11,7 +11,7 @@ export default function ThemesProvider({
   return (
     <NextThemesProvider
       attribute="class"
-      defaultTheme="system"
+      defaultTheme="dark"
       enableSystem
       disableTransitionOnChange
       enableColorScheme
